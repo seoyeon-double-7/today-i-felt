1187f56103119b559c34a9149816db781e6fddfc
cindy0817-web <s2103@e-mirim.hs.kr>
2022-10-22 11:16:43 +0900

    css 추가

---
+++ src/FeelingList/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.css';
+
+class FeelingList extends React.Component {
+  render() {
+    return (
+      <div className="feeling-list">
+        <h1>Feeling List</h1>
+      </div>
+    );
+  }
+}
+
+export default FeelingList;
