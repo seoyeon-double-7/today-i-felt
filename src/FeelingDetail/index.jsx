d8cad0d0386db49a2abc8e9c0f7bbd819f522e77
cindy0817-web <s2103@e-mirim.hs.kr>
2022-10-29 10:28:18 +0900

    button에 active 추가

--- src/FeelingDetail/index.jsx
+++ src/FeelingDetail/index.jsx
@@ -14,7 +14,7 @@ class FeelingDetail extends React.Component {
             Felt
           </h1>
           <div className="owner-name">
-            <strong>Name : </strong>
+            <strong>Name:</strong>
             <input type="text" maxLength={5} />
           </div>
         </div>
