f9a1d53cf74350ed3cd8c798b425a888c2f579b4
cindy0817-web <s2103@e-mirim.hs.kr>
2022-11-12 11:59:25 +0900

    addFeeling 페이지에 요소 추가

--- src/AddFeeling/index.jsx
+++ src/AddFeeling/index.jsx
@@ -3,7 +3,20 @@ import './style.css';
 
 class AddFeeling extends React.Component {
   render() {
-    return <h1>Add Feeling</h1>;
+    return (
+      <div className="add-feeling">
+        <div>날짜</div>
+        <div>2022. 11. 29</div>
+        <div>날씨</div>
+        <div>맑음</div>
+        <div>기분</div>
+        <div>
+          <input type="radio" />
+        </div>
+        <div>있었던 일</div>
+        <div className="what-happened"><textarea /></div>
+      </div>
+    );
   }
 }
 
